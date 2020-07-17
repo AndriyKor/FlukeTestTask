af8f9c2d06049e05b97a37409c002706dc9b2df8
AndriyKor <andriy.korovetskyy@gmail.com>
2020-07-17 20:11:10 +0300

    Initial commit

---
+++ Fluke.API/Controllers/Events.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fluke.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Events : ControllerBase
+    {
+        public string Get()
+        {
+            return "Hey there!";
+        }
+    }
+}
